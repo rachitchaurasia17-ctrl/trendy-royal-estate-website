114aead4dcc525881b86dd7453544c7ad6edc17a
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 22:36:43 +0530

    Fix: replace all template addresses with client address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for Trendy Roy
 
 Firm facts:
 - Firm: Trendy Royal Estate
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: Commercial Hub, Chandigarh City Center, Punjab
 - Phone: +91-00000-00000
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
